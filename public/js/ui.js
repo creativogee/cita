6f143f050e1c99d2eef6c033600a9606bfac8258
omowole gbenga <omowole.gbenga@gmail.com>
2021-08-25 22:14:21 +0100

    copy functionality

--- public/js/ui.js
+++ public/js/ui.js
@@ -1,31 +1,47 @@
-import convertCitation from './app.js'
+import convertCitation from "./app.js"
 
 const device = navigator.userAgent
-const citationInput = document.getElementById('rawCitation')
-const convertButton = document.getElementById('convertButton')
-const result = document.getElementById('result')
-const modal = document.getElementById('modal')
-const hint = document.getElementById('hint')
+const citationInput = document.getElementById("rawCitation")
+const convertButton = document.getElementById("convertButton")
+const copyButton = document.getElementById("copyButton")
+const result = document.getElementById("result")
+const modal = document.getElementById("modal")
+const hint = document.getElementById("hint")
 
-convertButton.addEventListener('click', async (evt) => {
+convertButton.addEventListener("click", async evt => {
   evt.preventDefault()
-  const {authoursAndYear, title, journal, edition, pages, error} = convertCitation(citationInput.value)
+  const { authoursAndYear, title, journal, edition, pages, error } = convertCitation(
+    citationInput.value
+  )
 
   if (authoursAndYear && title) {
-    result.innerHTML = `${authoursAndYear} ${title} <i>${journal}</i> <b>${edition}</b> ${pages}` 
+    result.innerHTML = `${authoursAndYear} ${title} <i>${journal}</i> <b>${edition}</b> ${pages}`
   } else {
-    if(citationInput.value === '') {
-      result.innerHTML = ''
+    if (citationInput.value === "") {
+      result.innerHTML = ""
     }
-    modal.classList.remove('invisible')
+    modal.classList.remove("invisible")
     modal.textContent = error
     setTimeout(() => {
-      modal.classList.add('invisible')
-    }, 3000) 
+      modal.classList.add("invisible")
+    }, 3000)
   }
 })
 
-citationInput.addEventListener('focusin', () => {
+copyButton.addEventListener("click", evt => {
+  evt.preventDefault()
+  const selection = window.getSelection()
+  const range = document.createRange()
+  range.selectNodeContents(result)
+  selection.removeAllRanges()
+  selection.addRange(range)
+  navigator.clipboard.writeText(result.textContent)
+  /** OR SIMPLY
+    navigator.clipboard.writeText(result.innerHTML
+  */
+})
+
+citationInput.addEventListener("focusin", () => {
   citationInput.select()
 })
 
